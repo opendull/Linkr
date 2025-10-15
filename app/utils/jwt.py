5aa5e51bd222d590bea76ee5c62092400ad7d8d2
opendull <1023155@comp.fcrit.ac.in>
2025-10-15 18:05:27 +0000

    Fix validate_token decorator and imports

--- app/utils/jwt.py
+++ app/utils/jwt.py
@@ -6,18 +6,17 @@ from app.models.token import Token
 def generate_token(user_id):
     expires = timedelta(hours=1)
     token = create_access_token(identity=user_id, expires_delta=expires)
-    expires_at = timedelta(hours=1) + datetime.utcnow()
+    expires_at = datetime.utcnow() + expires
     new_token = Token(user_id=user_id, token=token, expires_at=expires_at)
     db.session.add(new_token)
     db.session.commit()
     return token
 
-
 def validate_token():
-    @jwt_required()
     def decorator(f):
+        @jwt_required()
         def wrapper(*args, **kwargs):
-            token = get_jwt().get('jti')  # Use jti for uniqueness
+            token = get_jwt().get('jti')
             user_id = get_jwt_identity()
             tok = Token.query.filter_by(user_id=user_id, token=token).first()
             if not tok or not tok.is_valid or tok.revoked_at or tok.expires_at < datetime.utcnow():
